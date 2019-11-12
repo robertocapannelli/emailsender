57c8d3c5dcb5d13ca6640ed9a135fd5fcf84dc9b
Roberto Capannelli <roberto.capannelli@gmail.com>
2019-11-12 20:57:18 +0100

    Fix: adjusted the code to use dotenv

--- send_email.php
+++ send_email.php
@@ -2,12 +2,29 @@
 
 // Import PHPMailer classes into the global namespace
 // These must be at the top of your script, not inside a function
+use Controller\HandleRequest;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 // Load Composer's autoloader
 require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv->load();
+
+$SENDER        = $_ENV['SENDER'];
+$SENDER_NAME   = $_ENV['SENDER_NAME'];
+$RECEIVER      = $_ENV['RECEIVER'];
+$RECEIVER_NAME = $_ENV['RECEIVER_NAME'];
+
+$name  = isset( $_POST['name'] ) ? $_POST['name'] : '';
+$email = isset( $_POST['email'] ) ? $_POST['email'] : '';
+$phone = isset( $_POST['phone'] ) ? $_POST['phone'] : '';
+
+
+$request     = new HandleRequest();
+$get_request = $request->createRequest( $name, $email, $phone );
 
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer( true );
@@ -24,8 +41,8 @@ try {
 	$mail->Port       = 465;        */                            // TCP port to connect to
 
 	//Recipients
-	$mail->setFrom( 'info@conte2007.it', 'Conte Orologi' );
-	$mail->addAddress( 'roberto.capannelli@gmail.com', 'Roberto Capannelli' );     // Add a recipient
+	$mail->setFrom( $SENDER, $SENDER_NAME );
+	$mail->addAddress( $RECEIVER, $RECEIVER_NAME );     // Add a recipient
 	//$mail->addAddress( 'ellen@example.com' );               // Name is optional
 	//$mail->addReplyTo( 'info@conteorologi.it', 'Information' );
 	//$mail->addCC( 'roberto@nwdesigns.it' );
@@ -38,7 +55,9 @@ try {
 	// Content
 	$mail->isHTML( true );                                  // Set email format to HTML
 	$mail->Subject = 'Here is the subject';
-	$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+	$mail->Body    = 'Hello ' . $get_request->getName() . ' your email is ' . $get_request->getEmail() . ' and your phone is ' . $get_request->getPhone();
+
+
 	$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 	$mail->send();
