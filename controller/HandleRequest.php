af9d689103b4152882a8ab5ae8841746bba74ae6
Roberto Capannelli <roberto.capannelli@gmail.com>
2019-11-12 10:41:56 +0100

    new: create controller to handle the requests

--- controller/HandleRequest.php
+++ controller/HandleRequest.php
@@ -2,7 +2,26 @@
 
 namespace Controller;
 
+use Model\Request;
+
 class HandleRequest {
 
+	private $request;
+
+	/**
+	 * Create a request from user data
+	 *
+	 * @TODO this should validate and sanitize data
+	 * @param $name
+	 * @param $email
+	 * @param $phone
+	 *
+	 * @return Request
+	 */
+	public function createRequest( $name, $email, $phone) {
+		$this->request = new Request( $name, $email, $phone );
+
+		return $this->request;
+	}
 
 }
