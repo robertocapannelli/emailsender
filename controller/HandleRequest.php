d0ea0c96e22b2ea0627385dd40db583b7295da71
Roberto Capannelli <roberto.capannelli@gmail.com>
2019-11-12 20:56:37 +0100

    Update: create a request in the controller

--- controller/HandleRequest.php
+++ controller/HandleRequest.php
@@ -12,13 +12,23 @@ class HandleRequest {
 	 * Create a request from user data
 	 *
 	 * @TODO this should validate and sanitize data
+	 *
 	 * @param $name
 	 * @param $email
 	 * @param $phone
 	 *
 	 * @return Request
 	 */
-	public function createRequest( $name, $email, $phone) {
+	public function createRequest( $name, $email, $phone ) {
+
+		if ( ! empty( $name ) && ! empty( $email ) && ! empty( $phone ) ) {
+			$name  = filter_var( trim( $name ), FILTER_SANITIZE_STRING );
+			$email = filter_var( trim( $email ), FILTER_SANITIZE_EMAIL );
+			$phone = filter_var( trim( $phone ), FILTER_SANITIZE_NUMBER_INT );
+		} else {
+			return null;
+		}
+
 		$this->request = new Request( $name, $email, $phone );
 
 		return $this->request;
