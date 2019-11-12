e9b79267476af7561964a816cfe9c160fa6afb25
Roberto Capannelli <roberto.capannelli@gmail.com>
2019-11-12 20:56:13 +0100

    New: added a simple html form

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
-<?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-use Model\Request;
-
-$request = new Request( "Ciccio", "ciao@ciccio.com", "234567889" );
-
-echo $request->getName();
+<!doctype html>
+<html lang="">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Your title here</title>
+	<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<form enctype="multipart/form-data" role="form" method="post" action="send_email.php">
+		<input type="text" name="name" placeholder="Type your name" required>
+		<br>
+		<input type="email" name="email" placeholder="Type your email" required>
+		<br>
+		<input type="tel" name="phone" placeholder="Type your phone number" required>
+		<br>
+		<input type="submit" value="Send">
+	</form>
+</body>
+</html>
 
 
