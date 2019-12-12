c46d0f1bdce93e05fd9131eec9a766918ee71a35
Roberto Capannelli <roberto.capannelli@gmail.com>
2019-12-12 15:43:50 +0100

    Changed some values

--- controller/HandleRequest.php
+++ controller/HandleRequest.php
@@ -132,10 +132,10 @@ class HandleRequest {
 			//Server settings
 			/*$mail->SMTPDebug = SMTP::DEBUG_LOWLEVEL;                      // Enable verbose debug output
 			$mail->isSMTP();                                            // Send using SMTP
-			$mail->Host       = 'smtps.aruba.it';                    // Set the SMTP server to send through
+			$mail->Host       = 'smtp.server';                    // Set the SMTP server to send through
 			$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-			$mail->Username   = 'info@conte2007';                     // SMTP username
-			$mail->Password   = '3G6!shU2C';                               // SMTP password
+			$mail->Username   = 'smtp username';                     // SMTP username
+			$mail->Password   = 'password';                               // SMTP password
 			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
 			$mail->Port       = 465;        */                            // TCP port to connect to
 
